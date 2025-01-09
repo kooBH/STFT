54fa82b8e2c9340b4b12ac8a6c77924ce6c03cab
KooBH <ninie4409@sogang.ac.kr>
2025-01-09 16:34:27 +0900

    n_fft != n_win case

--- cpp/WindowFunction.h
+++ cpp/WindowFunction.h
@@ -14,7 +14,7 @@ class WindowFunction {
 protected:
   const double pi = 3.141592653589793;
 
-  int frame_size, shift_size;
+  int frame_size, shift_size, window_length;
   bool norm;
   bool sym;
 
@@ -26,7 +26,7 @@ protected:
   void normalize();
 
 public:
-  inline WindowFunction(int frame_size, int shift_size, bool norm = true, bool sym = true);
+  inline WindowFunction(int frame_size, int shift_size, int window_length=-1, bool norm = true, bool sym = true);
   inline ~WindowFunction();
   // 2D
   inline void Process(double** buf, int channels);
@@ -44,15 +44,15 @@ public:
 
 class HammingWindow : public WindowFunction {
 public:
-  HammingWindow(int frame_size_, int shift_size_ = -1, double alpha_ = 0.54, double beta_ = 0.46, bool norm_ = true) :WindowFunction(frame_size_,shift_size_, norm_,true) {
+  HammingWindow(int frame_size_, int shift_size_ = -1, int window_length_=-1, double alpha_ = 0.54, double beta_ = 0.46, bool norm_ = true) :WindowFunction(frame_size_,shift_size_, window_length_, norm_,true) {
     int i;
     double tmp = 0;
     // default 25% overlap
     if (shift_size == -1) {
       shift_size = frame_size / 4;
     }
-    for (i = 0; i < frame_size; i++)
-      window[i] = alpha_ - beta_ * cos(2.0 * pi * (i / (double)frame_size));
+    for (i = 0; i < window_length; i++)
+      window[i] = alpha_ - beta_ * cos(2.0 * pi * (i / (double)window_length));
     if (norm)normalize();
   }
 };
@@ -60,7 +60,7 @@ public:
 class HannWindow : public HammingWindow {
 
 public:
-  HannWindow(int frame_size, int shift_size = -1, bool norm = true) :HammingWindow(frame_size, shift_size, 0.5, 0.5, norm) {
+  HannWindow(int frame_size_, int shift_size_ = -1, int window_length_=-1,bool norm_ = true) :HammingWindow(frame_size_, shift_size_,window_length_, 0.5, 0.5, norm_) {
   }
 };
 
@@ -68,15 +68,15 @@ class SineWindow : public WindowFunction {
 private:
 
 public:
-  SineWindow(int frame_size, int shift_size = -1, bool norm = true) : WindowFunction(frame_size, norm) {
+  SineWindow(int frame_size_, int shift_size_ = -1, int window_length_=-1, bool norm_ = true) : WindowFunction(frame_size_, shift_size_, window_length_, norm_) {
     int i;
     // default 50% overlap
-    if (shift_size == -1) {
-      shift_size = frame_size / 2;
+    if (shift_size_ == -1) {
+      shift_size = frame_size_ / 2;
     }
 
-    for (i = 0; i < frame_size; i++)
-      window[i] = sin(pi * ((double)i + 0.5) / (double)frame_size);
+    for (i = 0; i < window_length; i++)
+      window[i] = sin(pi * ((double)i + 0.5) / (double)window_length);
     if(norm)normalize();
 
   }
@@ -85,20 +85,29 @@ public:
 };
 
 
-inline WindowFunction::WindowFunction(int frame_size_, int shift_size_, bool norm_, bool sym_) {
+inline WindowFunction::WindowFunction(int frame_size_, int shift_size_,int window_length_, bool norm_, bool sym_) {
   int i;
   double tmp = 0;
   frame_size = frame_size_;
   shift_size = shift_size_;
+  window_length = window_length_;
   norm = norm_;
   sym = sym_;
 
-  window = new double[frame_size];
-  memset(window, 0, sizeof(double) * frame_size);
+  if(window_length == -1)
+    window_length = frame_size;
+
+  if (frame_size < window_length) {
+    printf("ERROR::frame_size < widnow_length case is not implemented.\n");
+    exit(-1);
+  }
+
+  window = new double[window_length];
+  memset(window, 0, sizeof(double) * window_length);
 
   if (!sym) {
-    synthesis_window = new double[frame_size];
-    memset(synthesis_window, 0, sizeof(double) * frame_size);
+    synthesis_window = new double[window_length];
+    memset(synthesis_window, 0, sizeof(double) * window_length);
   }
 
 }
@@ -111,11 +120,11 @@ inline WindowFunction::~WindowFunction() {
 
 inline void WindowFunction::normalize() {
   double tmp = 0.0;
-  for (int i = 0; i < frame_size; i++)
+  for (int i = 0; i < window_length; i++)
     tmp += window[i] * window[i];
   tmp /= shift_size;
   tmp = std::sqrt(tmp);
-  for (int i = 0; i < frame_size; i++)
+  for (int i = 0; i < window_length; i++)
     window[i] /= tmp;
 
 }
